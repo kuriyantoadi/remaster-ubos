f10f9d09d0a05bfe2d7da4b97a9579feaf483eff
kuriyantoadi <melatikuning212@gmail.com>
2018-08-31 17:25:53 +0700

    update 31 agustus 2018.4

--- install-sistem/install.sh
+++ install-sistem/install.sh
@@ -18,7 +18,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # update repository
-sudo apt update -y && sudo apt dist-upgrade &&
+sudo apt update -y
+sudo apt dist-upgrade 
 sudo apt install vlc -y &&
 sudo apt install audacious -y &&
 sudo apt install inkscape -y &&
